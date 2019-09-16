e840ffdbf93318a11730914477b8a0b21fead5ad
jhickey1021 <54514115+jhickey1021@users.noreply.github.com>
2019-09-16 09:51:11 -0400

    created form for patientRecordApp

---
+++ app/public/js/record.app.js
@@ -0,0 +1,22 @@
+var waitingApp = new Vue({
+  el: '#patientRecordApp',
+  data: {
+    patients: []
+  },
+  methods: {
+    fetchPatients() {
+      fetch('dummy.php')
+      .then(response => response.json())
+      .then(json => {patientRecordApp.patients = json});
+
+      // Means the same at this
+      // fetch('https://randomuser.me/api/')
+      // .then(function(response) {return response.json()})
+      // .then(function(json) {waitingApp.people = json});
+
+    }
+  },
+  created() {
+    this.fetchPatients()
+  }
+});
