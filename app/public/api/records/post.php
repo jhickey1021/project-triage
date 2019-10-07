cf6c488a61c1ee8199b85dedf197c01583290f56
jhickey1021 <54514115+jhickey1021@users.noreply.github.com>
2019-10-07 14:08:34 -0400

    adding guid to project triage

---
+++ app/public/api/records/post.php
@@ -0,0 +1,30 @@
+<?php
+
+// Step 0: Validate the incoming data
+use Ramsey\Uuid\Uuid;
+
+
+// Step 1: Get a datase connection from our help class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$stmt = $db->prepare(
+  'INSERT INTO Patient (patientGuid, firstName, lastName, dob, sexAtBirth)
+  VALUES (?, ?, ?, ?, ?)'
+);
+
+$guid = Uuid:: uuid4()->toString(); // i.e. 25769c6c-d34d-4bfe-ba98-e0ee856f3e7a
+
+$stmt->execute([
+
+  $guid,
+  $_POST['patientGuid'],
+  $_POST['firstName'],
+  $_POST['lastName'],
+  $_POST['dob'],
+  $_POST['sexAtBirth']
+]);
+
+// Step 4: Output
+header('HTTP/1.1 303 See Other');
+header('Location: ../records/');
